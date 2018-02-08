76c12513f973f687893a683768e44a2da8ece52b
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-02-08 17:38:25 +0530

    Basic Vuforia AR Implemented.

--- Assets/Scripts/CanvasHandler/MainMenuHandler.cs
+++ Assets/Scripts/CanvasHandler/MainMenuHandler.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour {
 
-	
+	public Button playBtn;
 	// Use this for initialization
 	void Start () {
-		
+
+		bindListeners();
 	}
 	
+
+	private void bindListeners(){
+		playBtn.onClick.AddListener(()=>onClickPlay());
+	}
+	private void onClickPlay(){
+		
+	}
 	// Update is called once per frame
 	void Update () {
 		
