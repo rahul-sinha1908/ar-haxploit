39acaeeaf8bc67c4f968c7d4a0d36b0e55d59c6d
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-02-13 12:15:42 +0530

    Set up firebase and registration screen.

--- Assets/Scripts/CanvasHandler/MainMenuHandler.cs
+++ Assets/Scripts/CanvasHandler/MainMenuHandler.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Firebase.Unity;
+using Firebase.Auth;
 
 public class MainMenuHandler : MonoBehaviour {
 
 	public Button playBtn;
+	private FirebaseAuth auth;
+	private FirebaseUser user;
+
+	public static MainMenuHandler instance;
+
+	void Awake(){
+		instance=this;
+	}
+
 	// Use this for initialization
 	void Start () {
 		Screen.orientation=ScreenOrientation.Portrait;
+		InitializeFirebase();
 		bindListeners();
 	}
-	
 
 	private void bindListeners(){
 		playBtn.onClick.AddListener(()=>onClickPlay());
@@ -23,5 +34,28 @@ public class MainMenuHandler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void InitializeFirebase() {
+		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+		auth.StateChanged += AuthStateChanged;
+		AuthStateChanged(this, null);
+	}
+
+	void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+		if (auth.CurrentUser != user) {
+			bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+			if (!signedIn && user != null) {
+				Debug.Log("Signed out " + user.UserId);
+			}
+			user = auth.CurrentUser;
+			if (signedIn) {
+				Debug.Log("Signed in " + user.UserId);
+			}
+		}
+	}
+
+	public FirebaseAuth GetFirebaseAuth(){
+		return auth;
 	}
 }
---
+++ Assets/Scripts/MenuScripts/LoginScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Unity;
+using Firebase.Auth;
+
+public class LoginScript : MonoBehaviour {
+
+	public Text emailTxt, passTxt;
+	public Button loginBtn, registerBtn;
+	// Use this for initialization
+	void Start () {
+		bindListener();
+	}
+	private void bindListener(){
+		loginBtn.onClick.AddListener(()=>onClickLogin());
+		registerBtn.onClick.AddListener(()=>onClickRegister());
+	}
+	private void onClickLogin(){
+		FirebaseAuth auth = MainMenuHandler.instance.GetFirebaseAuth();
+		auth.CreateUserWithEmailAndPasswordAsync(emailTxt.text, passTxt.text).ContinueWith(task => {
+			if (task.IsCanceled) {
+				Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+				return;
+			}
+			if (task.IsFaulted) {
+				Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				return;
+			}
+
+			// Firebase user has been created.
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+				newUser.DisplayName, newUser.UserId);
+		});
+	}
+	private void onClickRegister(){
+
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
