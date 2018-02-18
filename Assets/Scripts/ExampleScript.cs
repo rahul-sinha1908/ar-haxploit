1d284fcb0a5a84013e18dbdf2d3654492b672d85
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-02-18 11:41:41 +0530

    Shifted to 2017.3.1f1

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,46 @@
+﻿// iOS gyroscope example
+//
+// Create a cube with camera vector names on the faces.
+// Allow the iPhone to show named faces as it is oriented.
+
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+	private Gyroscope gyro;
+	private bool gyroEnabled;
+
+	private GameObject cameraHolder;
+	private Quaternion rot;
+
+	void Start(){
+		cameraHolder=new GameObject("Camera Holder");
+		cameraHolder.transform.position=transform.position;
+		transform.SetParent(cameraHolder.transform);
+
+		gyroEnabled=enableGyro();
+	}
+
+	bool enableGyro(){
+		if(SystemInfo.supportsGyroscope){
+			Debug.Log("Gyroscope is enabled");
+			gyro=Input.gyro;
+			gyro.enabled=true;
+
+			cameraHolder.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+			rot = new Quaternion(0,0,1,0);
+
+			return true;
+		}
+		Debug.Log("Gyroscope doesnt work");
+		return false;
+	}
+	void Update(){
+		if(gyroEnabled){
+			transform.localRotation = gyro.attitude*rot;
+			//transform.localRotation = Quaternion.LookRotation(gyro.gravity, Vector3.up);
+			Debug.Log("Show Details : "+gyro.gravity+" : "+gyro.attitude+" : "+transform.localRotation);
+			
+		}
+	}
+}
